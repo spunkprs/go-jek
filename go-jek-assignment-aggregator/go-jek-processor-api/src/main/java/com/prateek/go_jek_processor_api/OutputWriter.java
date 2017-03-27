7447de9e381266d12eabf173dec7bbe84535ab42
spunkprs <prateekraj1988@gmail.com>
2017-03-27 15:52:11 +0530

    removed unnecessary files

--- go-jek-assignment-aggregator/go-jek-processor-api/src/main/java/com/prateek/go_jek_processor_api/OutputWriter.java
+++
@@ -1,5 +0,0 @@
-package com.prateek.go_jek_processor_api;
-
-public interface OutputWriter {
-	void writeOutput(String output);
-}
--- go-jek-assignment-aggregator/go-jek-processor-api/src/main/java/com/prateek/go_jek_processor_api/ParkingEntryAndExitProcessor.java
+++
@@ -1,7 +0,0 @@
-package com.prateek.go_jek_processor_api;
-
-public interface ParkingEntryAndExitProcessor {
-
-	String parkingEntryProcess(String vehicleRegistrationNumber, String colour);
-	String parkingExitProcess(int slotNumber);
-}
